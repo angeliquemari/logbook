36c60f9b60b48b702ae5de3cf2c432ffc9008ca1
Angelique Nehmzow <angelique@nehmzow.com>
2019-04-23 01:33:37 -0700

    adding models

--- logbook/models.py
+++ logbook/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Diver(models.Model):
+    account = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    certification_org = models.CharField(max_length=100)
+    certification_level = models.CharField(max_length=100)
+    certification_date = models.DateField()
+
+class Dive(models.Model):
+    diver = models.ForeignKey(Diver, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100)
+    date = models.DateField()
+    start_time = models.TimeField()
+    duration = models.DurationField()
+    max_depth = models.IntegerField()
+    buddy_name = models.CharField(max_length=100)
+    log_text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
