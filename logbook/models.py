132549fcf4bd102db83ffb85935ce6318c66afee
Angelique Nehmzow <angelique@nehmzow.com>
2019-04-25 00:16:06 -0700

    adding date of last dive method to diver

--- logbook/models.py
+++ logbook/models.py
@@ -8,8 +8,14 @@ class Diver(models.Model):
     certification_level = models.CharField(max_length=100)
     certification_date = models.DateField()
 
+    # def __str__(self):
+    #     return self.account.username
+
+    def get_date_of_last_dive(self):
+        return self.dives.latest('date').date
+
 class Dive(models.Model):
-    diver = models.ForeignKey(Diver, on_delete=models.CASCADE)
+    diver = models.ForeignKey(Diver, on_delete=models.CASCADE, related_name='dives')
     location = models.CharField(max_length=100)
     date = models.DateField()
     start_time = models.TimeField()
@@ -18,3 +24,6 @@ class Dive(models.Model):
     buddy_name = models.CharField(max_length=100)
     log_text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return '%s %s' % (self.date, self.location)
